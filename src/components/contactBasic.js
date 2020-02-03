d10fe403a276619802ae396d847dd987f6888bbe
Mason Balbera <mhbalbera@gmail.com>
2020-02-03 11:41:32 -0500

    change contact funcitonal component

--- src/components/contactBasic.js
+++ src/components/contactBasic.js
@@ -1,36 +1,13 @@
 import React from 'react'
 import SEO from './seo'
 
-class Contact extends React.Component {
-    state = {
-        name: "",
-        email: "",
-        body: ""
-    }
-
-    handleChange = (event) => {
-        this.setState({
-            [event.target.name]: event.target.value
-        });
-    }
-
-    handleSubmit = (event) => {
-        event.preventDefault();
-
-        this.setState({
-            name: "",
-            email: "",
-            body: ""
-        })
-    }
-
-    render() {
-        return (
-            <div>
-                <a HREF="mailto:name@mydomain.com">Click Here To Email Me</a>                       
-            </div>
-        )
-    }
+const Contact = () => {
+    return (
+        <div>
+            <a HREF="mailto:mhbalbera@gmail.com">Click Here To Email Me!</a>                       
+        </div>
+    )
+        
 }
 
 export default Contact
